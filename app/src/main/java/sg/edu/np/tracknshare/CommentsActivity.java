904a45fceab2a56dca840c7f32e84a9be212d43e
mahshuk410 <s10204884@connect.np.edu.sg>
2021-06-21 16:14:52 +0800

    Modify Comments recyclerView

--- app/src/main/java/sg/edu/np/tracknshare/CommentsActivity.java
+++ app/src/main/java/sg/edu/np/tracknshare/CommentsActivity.java
@@ -2,10 +2,21 @@ package sg.edu.np.tracknshare;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import sg.edu.np.tracknshare.adapters.CommentsAdapter;
+import sg.edu.np.tracknshare.models.Comment;
 
 public class CommentsActivity extends AppCompatActivity {
     ImageView UserImg;
@@ -14,7 +25,7 @@ public class CommentsActivity extends AppCompatActivity {
     TextView PostCaption;
     TextView Likes;
     ImageView PostImg;
-
+    ArrayList<Comment> comments = new ArrayList<Comment>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +40,29 @@ public class CommentsActivity extends AppCompatActivity {
         PostDate.setText(receivePost.getStringExtra("Postdate"));
         Likes.setText(""+receivePost.getIntExtra("PostLikes",0));
         PostCaption.setText(receivePost.getStringExtra("PostCaption"));
+        comments = generateComments();
+        RecyclerView comments_rv = findViewById(R.id.rv_comments);
+        CommentsAdapter adapter = new CommentsAdapter(getApplicationContext(),comments);
+        LinearLayoutManager lm = new LinearLayoutManager(getApplicationContext());
+        comments_rv.setAdapter(adapter);
+        comments_rv.setLayoutManager(lm);
+        adapter.notifyDataSetChanged();
+    }
 
+    private ArrayList<Comment> generateComments() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy") ;
+        String date = sdf.format(calendar.getTime());
+        ArrayList<Comment> commentList = new ArrayList<Comment>();
+        for(int i = 1;i<=10;i++){
+
+            Comment cmt = new Comment();
+            cmt.setId(i);
+            cmt.setUserName("User"+i);
+            cmt.setDate(date);
+            cmt.setCommentText("This is comment #"+i);
+            commentList.add(cmt);
+        }
+        return commentList;
     }
 }
--- app/src/main/java/sg/edu/np/tracknshare/models/Comment.java
+++ app/src/main/java/sg/edu/np/tracknshare/models/Comment.java
@@ -1,11 +1,64 @@
 package sg.edu.np.tracknshare.models;
 
+import android.graphics.Bitmap;
+import android.media.Image;
+
 import java.util.Date;
 
 public class Comment {
-    private String Id;
+    private int Id;
+//    private Image UserImg;
     private String UserId;
+    private int PostId;
     private String UserName;
-    private java.util.Date Date;
+    private String Date;
     private String CommentText;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public int getPostId() {
+        return PostId;
+    }
+
+    public void setPostId(int postId) {
+        PostId = postId;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getCommentText() {
+        return CommentText;
+    }
+
+    public void setCommentText(String commentText) {
+        CommentText = commentText;
+    }
 }
