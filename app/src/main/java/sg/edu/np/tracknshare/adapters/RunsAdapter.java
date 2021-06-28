ab863c0254a23c4bb2f86a9d07b46fab45d3a68b
mahshuk410 <s10204884@connect.np.edu.sg>
2021-06-28 13:58:38 +0800

    create alert dialog for post sharing

--- app/src/main/java/sg/edu/np/tracknshare/adapters/RunsAdapter.java
+++ app/src/main/java/sg/edu/np/tracknshare/adapters/RunsAdapter.java
@@ -1,12 +1,17 @@
 package sg.edu.np.tracknshare.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -16,11 +21,19 @@ import sg.edu.np.tracknshare.models.Runs;
 import sg.edu.np.tracknshare.viewholders.RunsViewHolder;
 
 public class RunsAdapter extends RecyclerView.Adapter<RunsViewHolder> {
+    FragmentActivity fragmentActivity; //getActivity() object passed from RunFragment to builder AlertDialog
     Context context;
     ArrayList<Runs> runs;
-    public RunsAdapter(Context c,ArrayList<Runs> runsArrayList){
+    ArrayList<String> selectedSharingOptions;
+    /*sharing options will be added to this list
+   as list cannot be returned in AlertDialog.builder method*/
+
+    String[] sharingOptions = new String[]{"Share to TracknShare app","Share to other apps"};
+    //default options in alertDialog
+    public RunsAdapter(Context c,ArrayList<Runs> runsArrayList,FragmentActivity fActivity){
         context = c;
         runs = runsArrayList;
+        fragmentActivity = fActivity;
     }
     @NonNull
     @Override
@@ -31,7 +44,7 @@ public class RunsAdapter extends RecyclerView.Adapter<RunsViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull RunsViewHolder holder, int position) {
-        Drawable post_icon = context.getResources().getDrawable(R.drawable.writing);//post icon for every run
+        Drawable post_icon = context.getResources().getDrawable(R.drawable.share);//post icon for every run
         Runs r = runs.get(position);
         holder.MapImage.setImageResource(R.drawable.running_splash);
         holder.Run_Date.setText(r.getRunDate());
@@ -40,10 +53,72 @@ public class RunsAdapter extends RecyclerView.Adapter<RunsViewHolder> {
         holder.Run_timing.setText(""+r.getRunDuration());
         holder.Run_pace.setText(String.format("%,.2f",r.getRunPace()));
         holder.Post_Button.setImageDrawable(post_icon);
+        holder.Post_Button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                selectedSharingOptions = new ArrayList<String>();
+                openShareModal(sharingOptions);
+
+            }
+            private void openShareModal(String[] sharingOptions){
+                AlertDialog.Builder builder = new AlertDialog.Builder(fragmentActivity);
+                builder.setTitle("Choose your sharing option");
+                builder.setCancelable(true);
+                builder.setMultiChoiceItems(sharingOptions, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if(isChecked){
+                            selectedSharingOptions.add(sharingOptions[which]);
+                        } else {
+                            selectedSharingOptions.remove(sharingOptions[which]);
+
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setPositiveButton("Share", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if(selectedSharingOptions.size() > 0){
+                            Log.d("HELO","ghd");
+                            if(!selectedSharingOptions.isEmpty()){
+                                for (String option:selectedSharingOptions) {
+                                    if(option.equals(sharingOptions[0])){ // index 0 --> "Share to TracknShare app"
+                                        Log.d("HELO","jeff");
+                                        //Intent i = new Intent(context, PostActivity.class);
+/*
+                                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //to allow activity transition from adapter class
+                                        context.startActivity(i);
+*/
+
+                                    }
+                                    else{
+                                        //call sharesheet API
+                                    }
+                                }
+                            }
+
+                        }
+
+                    }
+
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return runs.size();
     }
+
 }
