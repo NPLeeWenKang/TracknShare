48a38022f3a520fcc7fc053127372402bf4f2ce0
mahshuk410 <s10204884@connect.np.edu.sg>
2021-06-29 12:14:07 +0800

    testing fragment file

--- app/src/main/java/sg/edu/np/tracknshare/fragments/CreatePostFragment.java
+++ app/src/main/java/sg/edu/np/tracknshare/fragments/CreatePostFragment.java
@@ -1,57 +1,84 @@
-package sg.edu.np.tracknshare.fragments;
+/*
+    package sg.edu.np.tracknshare.fragments;
 
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 
 import sg.edu.np.tracknshare.R;
 
+*/
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CreatePostFragment#newInstance} factory method to
  * create an instance of this fragment.
- */
-public class CreatePostFragment extends Fragment {
+ *//*
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class CreatePostFragment extends DialogFragment {
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
-
-    public CreatePostFragment() {
+    private Toolbar toolbar;
+    public CreatePostFragment () {
         // Required empty public constructor
+        //CreatePostFragment createPostFragment = new CreatePostFragment();
     }
 
-    /**
+    */
+/**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment CreatePostFragment.
-     */
+     *//*
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_post, container, false);
+        return view;
     }
-}
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar = view.findViewById(R.id.toolbar);
+        //toolbar.inflateMenu(R.menu.menu_post);
+        toolbar.setNavigationOnClickListener(v -> dismiss());
+        toolbar.setTitle("New Post");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null){
+            dialog.getWindow().setLayout(1,1); //Dialog MATCH PARENT for both width and height
+        }
+    }
+}*/
