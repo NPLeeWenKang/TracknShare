2357af155904b117b83e48fe33e84fa71394a939
mahshuk410 <s10204884@connect.np.edu.sg>
2021-06-30 10:39:33 +0800

    create ProfileFragment

--- app/src/main/java/sg/edu/np/tracknshare/fragments/ProfileFragment.java
+++ app/src/main/java/sg/edu/np/tracknshare/fragments/ProfileFragment.java
@@ -2,6 +2,8 @@ package sg.edu.np.tracknshare.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -62,5 +64,11 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
     }
 }
