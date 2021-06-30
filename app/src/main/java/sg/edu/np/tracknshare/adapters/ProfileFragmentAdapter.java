353af1692104de46e14dcc807044433e65d4edf7
mahshuk410 <s10204884@connect.np.edu.sg>
2021-06-30 22:04:29 +0800

    set the viewpager2 tabs and fragments for fragment_profile

---
+++ app/src/main/java/sg/edu/np/tracknshare/adapters/ProfileFragmentAdapter.java
@@ -0,0 +1,33 @@
+package sg.edu.np.tracknshare.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import sg.edu.np.tracknshare.fragments.fragment_mypost;
+import sg.edu.np.tracknshare.fragments.fragment_statistics;
+
+public class ProfileFragmentAdapter extends FragmentStateAdapter {
+    public ProfileFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position){
+            case 0:
+                return new fragment_mypost();
+            case 1:
+                return new fragment_statistics();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
--- app/src/main/java/sg/edu/np/tracknshare/fragments/fragment_statistics.java
+++ app/src/main/java/sg/edu/np/tracknshare/fragments/fragment_statistics.java
@@ -2,28 +2,20 @@ package sg.edu.np.tracknshare.fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
 import sg.edu.np.tracknshare.R;
-import sg.edu.np.tracknshare.adapters.PostFragmentsAdapter;
-import sg.edu.np.tracknshare.adapters.ProfileFragmentAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
+ * Use the {@link fragment_statistics#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class fragment_statistics extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +26,7 @@ public class ProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ProfileFragment() {
+    public fragment_statistics() {
         // Required empty public constructor
     }
 
@@ -44,11 +36,11 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
+     * @return A new instance of fragment fragment_statistics.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
+    public static fragment_statistics newInstance(String param1, String param2) {
+        fragment_statistics fragment = new fragment_statistics();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -69,25 +61,6 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        ProfileFragmentAdapter profileFragmentAdapter = new ProfileFragmentAdapter(getChildFragmentManager(),getLifecycle());
-        ViewPager2 viewPager2 = view.findViewById(R.id.profilePager);
-        viewPager2.setAdapter(profileFragmentAdapter);
-        viewPager2.setSaveEnabled(false);
-        TabLayout tabLayout = view.findViewById(R.id.profiletabLayout);
-
-        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) { }
-
-        }).attach();
-        tabLayout.getTabAt(0).setText("My Posts");
-        tabLayout.getTabAt(1).setText("My Statistics");
-
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
     }
 }
