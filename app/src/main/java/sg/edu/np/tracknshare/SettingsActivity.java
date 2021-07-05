909c73880ca2dc0bd49ad03dcd44a61c01f52ff3
WenKang <wenkang11861@gmail.com>
2021-07-05 15:46:46 +0800

    implemented view for settings

---
+++ app/src/main/java/sg/edu/np/tracknshare/SettingsActivity.java
@@ -0,0 +1,43 @@
+package sg.edu.np.tracknshare;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Account Creation");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                overridePendingTransition(R.anim.end_enter, R.anim.end_exit);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.end_enter, R.anim.end_exit);
+    }
+}
--- app/src/main/java/sg/edu/np/tracknshare/handlers/StorageHandler.java
+++ app/src/main/java/sg/edu/np/tracknshare/handlers/StorageHandler.java
@@ -1,6 +1,7 @@
 package sg.edu.np.tracknshare.handlers;
 
 import android.graphics.Bitmap;
+import android.widget.ImageView;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -11,11 +12,15 @@ public class StorageHandler {
     private final FirebaseStorage storage = FirebaseStorage.getInstance();
     private final StorageReference storageRef = storage.getReference();
     public StorageHandler(){}
+
     public void UploadRunImage(Bitmap bm){
         StorageReference imagesRef = storageRef.child("images");
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] data = baos.toByteArray();
         imagesRef.child("1").putBytes(data);
+    }
+    public void LoadFileToApp(String imageId, ImageView imageRef){
+
     }
 }
