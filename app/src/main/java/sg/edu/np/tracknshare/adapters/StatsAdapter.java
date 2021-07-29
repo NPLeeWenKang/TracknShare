4197108ad9b8aeb113d15bb6c53731de2d4e4588
mahshuk410 <s10204884@connect.np.edu.sg>
2021-07-30 02:44:39 +0800

    Implement CardView for Statistics

--- app/src/main/java/sg/edu/np/tracknshare/adapters/StatsAdapter.java
+++ app/src/main/java/sg/edu/np/tracknshare/adapters/StatsAdapter.java
@@ -1,11 +1,15 @@
 package sg.edu.np.tracknshare.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -17,6 +21,7 @@ import sg.edu.np.tracknshare.viewholders.StatsViewHolder;
 
 //this adapter is for the individual statistics viewholder not the statistics tab element
 public class StatsAdapter extends RecyclerView.Adapter<StatsViewHolder> {
+
     Context context;
     ArrayList<Statistics> data;
     public StatsAdapter(Context c,ArrayList<Statistics>statisticsList) {
@@ -31,6 +36,7 @@ public class StatsAdapter extends RecyclerView.Adapter<StatsViewHolder> {
         return new StatsViewHolder(view);
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public void onBindViewHolder(@NonNull StatsViewHolder holder, int position) {
         Statistics stat = data.get(position);
@@ -39,6 +45,34 @@ public class StatsAdapter extends RecyclerView.Adapter<StatsViewHolder> {
         holder.stat2number.setText(""+stat.Stat2number);
         holder.stat1header.setText(stat.Stat1Header);
         holder.stat2header.setText(stat.Stat2Header);
+        int color; //initialise switch case variables
+        Drawable icon;
+        switch(position){ //generate 3 random colors for cardViews
+            // and set the respective icons
+            case 0:
+                color = context.getResources().getColor(R.color.fossil);
+                holder.cardView.setBackgroundColor(color);
+                icon = context.getResources().getDrawable(R.drawable.ic_foot);//steps avatar
+                holder.cardIcon.setImageDrawable(icon);
+                break;
+            case 1:
+                color = context.getResources().getColor(R.color.lime);
+                holder.cardView.setBackgroundColor(color);
+                icon = context.getResources().getDrawable(R.drawable.ic_calories);//kcal symbol
+                holder.cardIcon.setImageDrawable(icon);
+
+                break;
+            case 2:
+                color = context.getResources().getColor(R.color.primaryLightColor);
+                holder.cardView.setBackgroundColor(color);
+                icon = context.getResources().getDrawable(R.drawable.ic_outline_pin_drop_24);//pin symbol
+                holder.cardIcon.setImageDrawable(icon);
+
+                break;
+
+
+        }
+
     }
 
     @Override
--- app/src/main/java/sg/edu/np/tracknshare/viewholders/StatsViewHolder.java
+++ app/src/main/java/sg/edu/np/tracknshare/viewholders/StatsViewHolder.java
@@ -1,15 +1,19 @@
 package sg.edu.np.tracknshare.viewholders;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import sg.edu.np.tracknshare.R;
 
 public class StatsViewHolder extends RecyclerView.ViewHolder {
     public View view;
+    public CardView cardView;
+    public ImageView cardIcon;
     public TextView Title;
     public TextView stat1number;
     public TextView stat2number;
@@ -19,6 +23,8 @@ public class StatsViewHolder extends RecyclerView.ViewHolder {
     public StatsViewHolder(@NonNull View itemView) {
         super(itemView);
         view = itemView;
+        cardView = view.findViewById(R.id.cardView);
+        cardIcon = view.findViewById(R.id.cardIcon);
         Title = view.findViewById(R.id.Title);
         stat1header = view.findViewById(R.id.stat1_header);
         stat2header = view.findViewById(R.id.stat2_header);
